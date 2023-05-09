cdb4183b8cd44d5537de29d55cadd9a75f1166ac
tomerp1812 <tomerp1812@gmail.com>
2023-05-09 10:27:56 +0300

    .

---
+++ teamchat/src/Chat.js
@@ -0,0 +1,13 @@
+import Button from './Button/Button';
+import BigBlock from './BigBlock';
+
+function Chat() {
+  return (
+    <>
+      <Button />
+      <BigBlock />
+    </>
+  );
+}
+
+export default Chat;
