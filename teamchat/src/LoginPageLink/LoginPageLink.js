815450d37dc9301270eedd99b83183cf9348cbba
IditMedizada <iditm9@gmail.com>
2023-05-09 10:21:34 +0300

    delete

--- teamchat/src/LoginPageLink/LoginPageLink.js
+++
@@ -1,9 +0,0 @@
-function LoginPageLink() {
-    return (
-        <div className="middlePosition">
-            <span className="psw ">Have an account? <a href="LoginPage.html">Log in</a> </span>
-        </div>
-    );
-}
-
-export default LoginPageLink;
